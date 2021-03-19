91f85bc51f61b8df9f4568e9b9217fef0d69ac2e
Chris Hambacher <hambcastlesen12@gmail.com>
2021-03-19 19:06:58 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 <meta charset="utf-8" />
 <title>Hello World</title>
 <script>
-    let header = <?php 
+    let header = "<?php 
         $headers = apache_request_headers();
         foreach($headers as $header => $value) {
-            echo "$header:$value\n";
+            echo "$header: $value\n";
         }
-    ?>;
+    ?>";
 
     document.querySelector("#headers").innerHTML = header; 
 
