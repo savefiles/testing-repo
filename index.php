2b828ceaf17846b71e796208cc2ed7ac44a3c596
Chris Hambacher <hambcastlesen12@gmail.com>
2021-03-19 19:03:29 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>Hello World</title>
+<script>
+    let header = <?php 
+        $headers = apache_request_headers();
+        foreach($headers as $header => $value) {
+            echo "$header:$value\n";
+        }
+    ?>;
+
+    document.querySelector("#headers").innerHTML = header; 
+
+
+</script>
+</head>
+<body>
+	<p id="headers">Ur info is mine :)</p>
+</body>
+</html>
